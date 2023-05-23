14f5073e163ed4d1836895f7e4591ab3eae6a327
Ryan Funk <77073464+RyanFunk2000@users.noreply.github.com>
2023-05-22 18:08:22 -0600

    added player attack

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,17 +9,21 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     //private Animator anim;
 
+    //private Rigidbody playerRb;
+
     private float dirX = 0f;
     private float dirY = 0f;
-    private string facing_angle = "D"; 
+    private string facing_angle = "D";
+    private int index = 0;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] float rollForce = 6.0f;
     private float addSpeedX = 0f;
     private float addSpeedY = 0f;
     private bool rolling = false;
+    private bool stopMovement = false;
     private float timeSinceAction = 0.0f;
     private float rollDuration = 8.0f / 14.0f;
-    //private float rollCurrentTime;
+    private float attackDuration = 0.25f;
 
     // Start is called before the first frame update
     private void Start()
@@ -39,33 +43,64 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(1) && timeSinceAction > rollDuration) // Roll
         {
             rolling = true;
-            if (dirX > 0)
+            if (dirX > 0f) // Right
             {
                 addSpeedX += rollForce;
             }
-            if (dirX < 0)
+            if (dirX < 0f) // Left
             {
                 addSpeedX -= rollForce;
             }
-            if (dirY > 0)
+            if (dirY > 0f) // Up
             {
                 addSpeedY += rollForce;
             }
-            if (dirY < 0)
+            if (dirY < 0f) // Down
             {
                 addSpeedY -= rollForce;
             }
             
             Invoke("ResetMovement", rollDuration);
+            // Reset timer
+            timeSinceAction = 0.0f;
+        }
+
+        if (Input.GetMouseButtonDown(0) && timeSinceAction > attackDuration && !rolling) // Attack
+        {
+            if (facing_angle == "X" && sprite.flipX == false) // Right
+            {
+                index = 0;
+            }
+            if (facing_angle == "X" && sprite.flipX == true) // Left
+            {
+                index = 1;
+            }
+            if (facing_angle == "U") // Up
+            {
+                index = 2;
+            }
+            if (facing_angle == "D") // Down
+            {
+                index = 3;
+            }
+
+            transform.GetChild(index).gameObject.SetActive(true);
+            stopMovement = true;
+            Invoke("ResetMovement", attackDuration);
 
             // Reset timer
             timeSinceAction = 0.0f;
         }
 
+        if (stopMovement)
+        {
+            dirX = 0f;
+            dirY = 0f;
+        }
         rb.velocity = new Vector2((dirX * moveSpeed) + addSpeedX, (dirY * moveSpeed) + addSpeedY);
         timeSinceAction += Time.deltaTime;
 
-        // UpdateAnimationState();
+        UpdateAnimationState();
 
     }
 
@@ -74,17 +109,20 @@ public class PlayerMovement : MonoBehaviour
         rolling = false;
         addSpeedX = 0f;
         addSpeedY = 0f;
+
+        stopMovement = false;
+        for (int i = 0; i < 4; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 
     private void UpdateAnimationState()
     {
 
-        if (Input.GetMouseButtonDown(0) && timeSinceAction > 0.25f && !rolling) // Attack
+        if (Input.GetMouseButtonDown(0) && timeSinceAction > attackDuration && !rolling) // Attack
         {
             // anim.Play("Player_Attack" + facing_angle);
-
-            // Reset timer
-            timeSinceAction = 0.0f;
         }
 
         else if (Input.GetMouseButtonDown(1) && timeSinceAction > rollDuration) // Roll
