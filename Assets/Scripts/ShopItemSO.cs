4b9663d24c0ca5c9a73f6db27c57058bf6baa8b1
Henry Nelson <60790820+HNelson98@users.noreply.github.com>
2023-05-31 13:38:21 -0600

    Scripts are in and working

--- Assets/Scripts/ShopItemSO.cs
+++ Assets/Scripts/ShopItemSO.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShopItemSC : MonoBehaviour
+[CreateAssetMenu(fileName = "shopMenu", menuName = "Scriptable Object/New Shop Item", order = 1)]
+public class ShopItemSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string title;
+    public string desc;
+    public int baseCost;
 }
