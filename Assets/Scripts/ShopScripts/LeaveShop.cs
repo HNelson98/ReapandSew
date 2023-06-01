3f91307de44c37c4156bda31fcd61dd342069da6
Henry Nelson <60790820+HNelson98@users.noreply.github.com>
2023-06-01 13:13:53 -0600

    Added an exit button

---
+++ Assets/Scripts/ShopScripts/LeaveShop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveShop : MonoBehaviour
+{
+    public void TransitionScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
